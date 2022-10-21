b42ccc44326208d31fab990bef836578f2a5604c
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-21 17:08:02 +0300

    Create poll.php

---
+++ poll.php
@@ -0,0 +1,37 @@
+PHP Example - AJAX Poll
+AJAX Poll
+The following example will demonstrate a poll where the result is shown without reloading.
+
+Do you like PHP and AJAX so far?
+Yes: 
+No: 
+Example Explained - The HTML Page
+When a user chooses an option above, a function called "getVote()" is executed. The function is triggered by the "onclick" event:
+
+<html>
+<head>
+<script>
+function getVote(int) {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+  xmlhttp.send();
+}
+</script>
+</head>
+<body>
+
+<div id="poll">
+<h3>Do you like PHP and AJAX so far?</h3>
+<form>
+Yes: <input type="radio" name="vote" value="0" onclick="getVote(this.value)"><br>
+No: <input type="radio" name="vote" value="1" onclick="getVote(this.value)">
+</form>
+</div>
+
+</body>
+</html>
